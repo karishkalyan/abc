71c74a38ad59c484f888565c63e2bb12ce1cb973
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-02-07 16:27:11 +0530

    string
    code

---
+++ string.h
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char ch[25];
+printf("\n enter the string:");
+scanf("%s",ch);
+if(a>o)
+ printf("\n yes");
+else
+ printf("\n no");
+getch();
+}
